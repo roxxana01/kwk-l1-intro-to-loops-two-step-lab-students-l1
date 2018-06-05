58a3ef34ebd000d755099ec357b1a1f85a5c2146
persia sarram <kittycuppers01@gmail.com>
2018-06-05 20:56:05 +0000

    automatically backed up by learn

---
+++ loops.rb
@@ -0,0 +1,57 @@
+# def dance 
+  
+#   moves_completed = 0
+  
+#   loop do
+#     if moves_completed == 10
+#       break
+#     end
+    
+#     puts "To the right, to the right"
+#     puts  "To the left, to the left"
+#     puts  "Now kick, kick"
+#     puts "Now walk it out"
+    
+#     moves_completed +=1
+#     puts "#{moves_completed} move(s) completed!"
+#   end #ends the loop
+
+# end #ends the method
+
+# dance
+
+
+def dance
+  moves_completed = 0
+  
+  while moves_completed < 10
+  puts  "To the left, to the left"
+  puts " walk it out"
+  
+  moves_completed += 1
+  #moves_completed = moves_completed + 1
+  
+puts "#{moves_completed} move(s) completed!"
+
+end #ends while
+
+end #ends method
+
+dance
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
